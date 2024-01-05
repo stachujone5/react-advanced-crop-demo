f68ba01990d576f6bf6f47a030092ad90b3929c9
Stanisław Jakubowski <sjakubowski3554@gmail.com>
2024-01-05 16:31:02 +0100

    Merge images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,9 @@ export default function Home() {
   const [height, setHeight] = useState(
     aspectRatioOptions["InstagramPost"].height
   );
+
+  const [src, setSrc] = useState<string | null>(null);
+
   const cropperRef = useRef<CropperRef>(null);
 
   useEffect(() => {
@@ -28,16 +31,73 @@ export default function Home() {
     setHeight(aspectRatioOptions[selectedService].height);
   }, [selectedService]);
 
+  useEffect(() => {
+    const img = new Image();
+
+    img.crossOrigin = "Anonymous"; // Needed if the image is served from a different domain
+    img.src =
+      "https://www.bearsforhumanity.com/cdn/shop/files/IMG_1792copy_1024x1024.jpg?v=1696342413";
+    img.onload = () => {
+      const canvas = document.createElement("canvas");
+      canvas.width = 1000;
+      canvas.height = 1000;
+
+      const ctx = canvas.getContext("2d");
+      if (!ctx) return;
+
+      ctx.fillStyle = "rgba(255, 255, 255, 0)"; // Transparent background
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      // Draw the image at the center
+      ctx.drawImage(
+        img,
+        (canvas.width - 500) / 2,
+        (canvas.height - 500) / 2,
+        500,
+        500
+      );
+
+      // Create a Blob URL from the canvas
+      canvas.toBlob((blob) => {
+        if (!blob) return;
+
+        const newSrc = URL.createObjectURL(blob);
+        setSrc(newSrc);
+      });
+    };
+  }, []);
+
+  if (!src) return null;
+
   return (
-    <main className="flex min-h-screen justify-center flex-col items-center p-24 gap-4">
-      <Cropper
-        ref={cropperRef}
-        src={"https://picsum.photos/500/500"}
-        stencilProps={{
-          aspectRatio:
-            selectedService !== "Custom" ? width / height : undefined,
+    <main className="flex min-h-screen justify-center flex-col items-center gap-4">
+      <div
+        style={{
+          width: `${300}px`,
+          height: `${300}px`,
+          overflow: "hidden",
+          position: "relative", // Make sure the position is relative to contain the absolutely positioned cropper within
         }}
-      />
+      >
+        <Cropper
+          ref={cropperRef}
+          src={src}
+          defaultVisibleArea={{
+            height: 500,
+            width: 500,
+            top: 250,
+            left: 250,
+          }}
+          defaultCoordinates={{
+            height: 200,
+            width: 200,
+          }}
+          stencilProps={{
+            aspectRatio:
+              selectedService !== "Custom" ? width / height : undefined,
+          }}
+        />
+      </div>
       <div className="flex flex-col">
         <label htmlFor="service-select">Service</label>
         <select
